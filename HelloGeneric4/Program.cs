e0376703e5e5f13ef98f9cba32fe259edacf6c95
zhuqinfei <1586859762@qq.com>
2024-02-06 23:34:16 +0800

    无返回值泛型委托

---
+++ HelloGeneric4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloGeneric4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //无返回值泛型委托
+            Action<string> a1 = Say;
+            a1("Timothy");
+            Action<int> a2 = Mul;
+            a2(1);
+
+        }
+
+        static void Say(string str)
+        {
+            Console.WriteLine($"Hello,{str}");
+        }
+        static void Mul(int x)
+        {
+            Console.WriteLine(x * 100);
+        }
+    }
+}
